1e5ff5d948aaeb1ebe0eaffcc44ab6189946dd37
julianguyn <julianguyenpt@gmail.com>
2024-10-15 12:16:01 -0400

    updated data file for References

--- R/data.R
+++ R/data.R
@@ -70,3 +70,196 @@ NULL
 #' @keywords datasets
 #' @format `data.frame` object
 NULL
+
+#' A `data.frame` containing a subset of cell lines and driver mutations from
+#' the Genomics of Drug Sensitivity in Cancer (GDSC) dataset.
+#'
+#' This is a subset of the GDSC driver mutations dataset from Iorio et al. 2016
+#' containing a cancer driver mutation events for a random selection of cell lines.
+#'
+#' @references
+#' 1. Yang, W., Soares, J., Greninger, P., Edelman, E. J., Lightfoot, H., Forbes, S., Bindal, N., Beare, D., Smith, J. A., Thompson, I. R., Ramaswamy, S., & Futreal, P. A. (2013). Genomics of Drug Sensitivity in Cancer (GDSC): a resource for therapeutic biomarker discovery in cancer cells. Nucleic Acids Research, 41(D1), D955–D961. https://doi.org/10.1093/nar/gks1111
+#' 2. Iorio, F., Knijnenburg, T. A., Vis, D. J., Bignell, G. R., Menden, M. P., Schubert, M., Aben, N., Gonçalves, E., Barthorpe, S., Lightfoot, H., Cokelaer, T., Greninger, P., van Dyk, E., Chang, H., de Silva, H., Heyn, H., Deng, X., Egan, R. K., Liu, Q., Mironenko, T., … Garnett, M. J. (2016). A Landscape of Pharmacogenomic Interactions in Cancer. Cell, 166(3), 740–754. https://doi.org/10.1016/j.cell.2016.06.017
+#' 3. Garnett, M. J., Edelman, E. J., Heidorn, S. J., Greenman, C. D., Dastur, A., Lau, K. W., Greninger, P., Thompson, I. R., Luo, X., Soares, J., Liu, Q., Iorio, F., Surdez, D., Chen, L., Milano, R. J., Bignell, G. R., Tam, A. T., Davies, H., Stevenson, J. A., … Benes, C. H. (2012). Systematic identification of genomic markers of drug sensitivity in cancer cells. Nature, 483(7391), 570–575. https://doi.org/10.1038/nature11005
+#'
+#' @docType data
+#' @name GDSC_drivers
+#' @usage data(GDSC_drivers)
+#' @keywords datasets
+#' @format `data.frame` object
+NULL
+
+#' A `data.frame` containing a subset of cell lines and RNA-seq data from
+#' the Genomics of Drug Sensitivity in Cancer (GDSC) dataset.
+#'
+#' This is a subset of the GDSC RNA-seq dataset from Iorio et al. 2016
+#' containing RNA-sequencing expression data (read counts and FPKM)
+#' for a random selection of cell lines.
+#'
+#' @references
+#' 1. Yang, W., Soares, J., Greninger, P., Edelman, E. J., Lightfoot, H., Forbes, S., Bindal, N., Beare, D., Smith, J. A., Thompson, I. R., Ramaswamy, S., & Futreal, P. A. (2013). Genomics of Drug Sensitivity in Cancer (GDSC): a resource for therapeutic biomarker discovery in cancer cells. Nucleic Acids Research, 41(D1), D955–D961. https://doi.org/10.1093/nar/gks1111
+#' 2. Iorio, F., Knijnenburg, T. A., Vis, D. J., Bignell, G. R., Menden, M. P., Schubert, M., Aben, N., Gonçalves, E., Barthorpe, S., Lightfoot, H., Cokelaer, T., Greninger, P., van Dyk, E., Chang, H., de Silva, H., Heyn, H., Deng, X., Egan, R. K., Liu, Q., Mironenko, T., … Garnett, M. J. (2016). A Landscape of Pharmacogenomic Interactions in Cancer. Cell, 166(3), 740–754. https://doi.org/10.1016/j.cell.2016.06.017
+#' 3. Garnett, M. J., Edelman, E. J., Heidorn, S. J., Greenman, C. D., Dastur, A., Lau, K. W., Greninger, P., Thompson, I. R., Luo, X., Soares, J., Liu, Q., Iorio, F., Surdez, D., Chen, L., Milano, R. J., Bignell, G. R., Tam, A. T., Davies, H., Stevenson, J. A., … Benes, C. H. (2012). Systematic identification of genomic markers of drug sensitivity in cancer cells. Nature, 483(7391), 570–575. https://doi.org/10.1038/nature11005
+#'
+#' @docType data
+#' @name GDSC_rnaseq
+#' @usage data(GDSC_rnaseq)
+#' @keywords datasets
+#' @format `data.frame` object
+NULL
+
+#' A `data.frame` containing the mapping of gene names used in the Genomics of
+#' Drug Sensitivity in Cancer (GDSC) dataset.
+#'
+#' This is an annotation file obtained from Iorio et al. 2016
+#' containing the mapping of gene names used in the GDSC dataset.
+#'
+#' @references
+#' 1. Yang, W., Soares, J., Greninger, P., Edelman, E. J., Lightfoot, H., Forbes, S., Bindal, N., Beare, D., Smith, J. A., Thompson, I. R., Ramaswamy, S., & Futreal, P. A. (2013). Genomics of Drug Sensitivity in Cancer (GDSC): a resource for therapeutic biomarker discovery in cancer cells. Nucleic Acids Research, 41(D1), D955–D961. https://doi.org/10.1093/nar/gks1111
+#' 2. Iorio, F., Knijnenburg, T. A., Vis, D. J., Bignell, G. R., Menden, M. P., Schubert, M., Aben, N., Gonçalves, E., Barthorpe, S., Lightfoot, H., Cokelaer, T., Greninger, P., van Dyk, E., Chang, H., de Silva, H., Heyn, H., Deng, X., Egan, R. K., Liu, Q., Mironenko, T., … Garnett, M. J. (2016). A Landscape of Pharmacogenomic Interactions in Cancer. Cell, 166(3), 740–754. https://doi.org/10.1016/j.cell.2016.06.017
+#' 3. Garnett, M. J., Edelman, E. J., Heidorn, S. J., Greenman, C. D., Dastur, A., Lau, K. W., Greninger, P., Thompson, I. R., Luo, X., Soares, J., Liu, Q., Iorio, F., Surdez, D., Chen, L., Milano, R. J., Bignell, G. R., Tam, A. T., Davies, H., Stevenson, J. A., … Benes, C. H. (2012). Systematic identification of genomic markers of drug sensitivity in cancer cells. Nature, 483(7391), 570–575. https://doi.org/10.1038/nature11005
+#'
+#' @docType data
+#' @name GDSC_gene_identifiers
+#' @usage data(GDSC_gene_identifiers)
+#' @keywords datasets
+#' @format `data.frame` object
+NULL
+
+#' A `data.frame` containing metadata for cell lines used in the Genomics of
+#' Drug Sensitivity in Cancer (GDSC) project.
+#'
+#' This is an annotation file obtained from Iorio et al. 2016
+#' containing a list of cell line models used, including metadata for each cell
+#' line.
+#'
+#' @references
+#' 1. Yang, W., Soares, J., Greninger, P., Edelman, E. J., Lightfoot, H., Forbes, S., Bindal, N., Beare, D., Smith, J. A., Thompson, I. R., Ramaswamy, S., & Futreal, P. A. (2013). Genomics of Drug Sensitivity in Cancer (GDSC): a resource for therapeutic biomarker discovery in cancer cells. Nucleic Acids Research, 41(D1), D955–D961. https://doi.org/10.1093/nar/gks1111
+#' 2. Iorio, F., Knijnenburg, T. A., Vis, D. J., Bignell, G. R., Menden, M. P., Schubert, M., Aben, N., Gonçalves, E., Barthorpe, S., Lightfoot, H., Cokelaer, T., Greninger, P., van Dyk, E., Chang, H., de Silva, H., Heyn, H., Deng, X., Egan, R. K., Liu, Q., Mironenko, T., … Garnett, M. J. (2016). A Landscape of Pharmacogenomic Interactions in Cancer. Cell, 166(3), 740–754. https://doi.org/10.1016/j.cell.2016.06.017
+#' 3. Garnett, M. J., Edelman, E. J., Heidorn, S. J., Greenman, C. D., Dastur, A., Lau, K. W., Greninger, P., Thompson, I. R., Luo, X., Soares, J., Liu, Q., Iorio, F., Surdez, D., Chen, L., Milano, R. J., Bignell, G. R., Tam, A. T., Davies, H., Stevenson, J. A., … Benes, C. H. (2012). Systematic identification of genomic markers of drug sensitivity in cancer cells. Nature, 483(7391), 570–575. https://doi.org/10.1038/nature11005
+#'
+#' @docType data
+#' @name GDSC_model_list
+#' @usage data(GDSC_model_list)
+#' @keywords datasets
+#' @format `data.frame` object
+NULL
+
+#' A `data.frame` containing methylation beta values for cell lines from the
+#' Genomics of Drug Sensitivity in Cancer (GDSC) dataset.
+#'
+#' This is a subset of the GDSC methylation dataset from Iorio et al. 2016
+#' containing methylation beta values for a random selection of cell lines.
+#'
+#' @references
+#' 1. Yang, W., Soares, J., Greninger, P., Edelman, E. J., Lightfoot, H., Forbes, S., Bindal, N., Beare, D., Smith, J. A., Thompson, I. R., Ramaswamy, S., & Futreal, P. A. (2013). Genomics of Drug Sensitivity in Cancer (GDSC): a resource for therapeutic biomarker discovery in cancer cells. Nucleic Acids Research, 41(D1), D955–D961. https://doi.org/10.1093/nar/gks1111
+#' 2. Iorio, F., Knijnenburg, T. A., Vis, D. J., Bignell, G. R., Menden, M. P., Schubert, M., Aben, N., Gonçalves, E., Barthorpe, S., Lightfoot, H., Cokelaer, T., Greninger, P., van Dyk, E., Chang, H., de Silva, H., Heyn, H., Deng, X., Egan, R. K., Liu, Q., Mironenko, T., … Garnett, M. J. (2016). A Landscape of Pharmacogenomic Interactions in Cancer. Cell, 166(3), 740–754. https://doi.org/10.1016/j.cell.2016.06.017
+#' 3. Garnett, M. J., Edelman, E. J., Heidorn, S. J., Greenman, C. D., Dastur, A., Lau, K. W., Greninger, P., Thompson, I. R., Luo, X., Soares, J., Liu, Q., Iorio, F., Surdez, D., Chen, L., Milano, R. J., Bignell, G. R., Tam, A. T., Davies, H., Stevenson, J. A., … Benes, C. H. (2012). Systematic identification of genomic markers of drug sensitivity in cancer cells. Nature, 483(7391), 570–575. https://doi.org/10.1038/nature11005
+#'
+#' @docType data
+#' @name GDSC_methylation
+#' @usage data(GDSC_methylation)
+#' @keywords datasets
+#' @format `data.frame` object
+NULL
+
+#' A `data.frame` containing GDSC methylation Sentrix array cell line metadata 
+#' for the Genomics of Drug Sensitivity in Cancer (GDSC) dataset.
+#'
+#' This is an annotation file obtained from Iorio et al. 2016
+#' containing metadata for cell lines used in the GDSC methylation dataset, 
+#' specifically containing information regarding the Sentrix arrays.
+#'
+#' @references
+#' 1. Yang, W., Soares, J., Greninger, P., Edelman, E. J., Lightfoot, H., Forbes, S., Bindal, N., Beare, D., Smith, J. A., Thompson, I. R., Ramaswamy, S., & Futreal, P. A. (2013). Genomics of Drug Sensitivity in Cancer (GDSC): a resource for therapeutic biomarker discovery in cancer cells. Nucleic Acids Research, 41(D1), D955–D961. https://doi.org/10.1093/nar/gks1111
+#' 2. Iorio, F., Knijnenburg, T. A., Vis, D. J., Bignell, G. R., Menden, M. P., Schubert, M., Aben, N., Gonçalves, E., Barthorpe, S., Lightfoot, H., Cokelaer, T., Greninger, P., van Dyk, E., Chang, H., de Silva, H., Heyn, H., Deng, X., Egan, R. K., Liu, Q., Mironenko, T., … Garnett, M. J. (2016). A Landscape of Pharmacogenomic Interactions in Cancer. Cell, 166(3), 740–754. https://doi.org/10.1016/j.cell.2016.06.017
+#' 3. Garnett, M. J., Edelman, E. J., Heidorn, S. J., Greenman, C. D., Dastur, A., Lau, K. W., Greninger, P., Thompson, I. R., Luo, X., Soares, J., Liu, Q., Iorio, F., Surdez, D., Chen, L., Milano, R. J., Bignell, G. R., Tam, A. T., Davies, H., Stevenson, J. A., … Benes, C. H. (2012). Systematic identification of genomic markers of drug sensitivity in cancer cells. Nature, 483(7391), 570–575. https://doi.org/10.1038/nature11005
+#'
+#' @docType data
+#' @name GDSC_methylation_model_list
+#' @usage data(GDSC_methylation_model_list)
+#' @keywords datasets
+#' @format `data.frame` object
+NULL
+
+#' A `data.frame` containing drug response data for the Genomics of Drug
+#' Sensitivity in Cancer (GDSC) dataset.
+#'
+#' This is a subset of drug repsonse dataset from Iorio et al. 2016
+#' containing cell line response (AUC, IC50) for a selection of drugs.
+#'
+#' @references
+#' 1. Yang, W., Soares, J., Greninger, P., Edelman, E. J., Lightfoot, H., Forbes, S., Bindal, N., Beare, D., Smith, J. A., Thompson, I. R., Ramaswamy, S., & Futreal, P. A. (2013). Genomics of Drug Sensitivity in Cancer (GDSC): a resource for therapeutic biomarker discovery in cancer cells. Nucleic Acids Research, 41(D1), D955–D961. https://doi.org/10.1093/nar/gks1111
+#' 2. Iorio, F., Knijnenburg, T. A., Vis, D. J., Bignell, G. R., Menden, M. P., Schubert, M., Aben, N., Gonçalves, E., Barthorpe, S., Lightfoot, H., Cokelaer, T., Greninger, P., van Dyk, E., Chang, H., de Silva, H., Heyn, H., Deng, X., Egan, R. K., Liu, Q., Mironenko, T., … Garnett, M. J. (2016). A Landscape of Pharmacogenomic Interactions in Cancer. Cell, 166(3), 740–754. https://doi.org/10.1016/j.cell.2016.06.017
+#' 3. Garnett, M. J., Edelman, E. J., Heidorn, S. J., Greenman, C. D., Dastur, A., Lau, K. W., Greninger, P., Thompson, I. R., Luo, X., Soares, J., Liu, Q., Iorio, F., Surdez, D., Chen, L., Milano, R. J., Bignell, G. R., Tam, A. T., Davies, H., Stevenson, J. A., … Benes, C. H. (2012). Systematic identification of genomic markers of drug sensitivity in cancer cells. Nature, 483(7391), 570–575. https://doi.org/10.1038/nature11005
+#'
+#' @docType data
+#' @name GDSC_drug_response
+#' @usage data(GDSC_drug_response)
+#' @keywords datasets
+#' @format `data.frame` object
+NULL
+
+#' A `data.frame` containing global chromatin profiling of histone marks for
+#' the Cancer Cell Line Encyclopedia (CCLE) dataset.
+#'
+#' This is a subset of the CCLE histone modifications dataset from Ghandi et al. 2019 
+#' for available cell lines.
+#'
+#' @references
+#' Ghandi, M., Huang, F. W., Jané-Valbuena, J., Kryukov, G. V., Lo, C. C., McDonald, E. R., Barretina, J., Gelfand, E. T., Bielski, C. M., Li, H., Hu, K., Andreev-Drakhlin, A. Y., Kim, J., Hess, J. M., Haas, B. J., Aguet, F., Weir, B. A., Rothberg, M. V., Paolella, B. R., … Golub, T. R. (2019). Next-generation characterization of the Cancer Cell Line Encyclopedia. Nature, 569(7757), 503–508. https://doi.org/10.1038/s41586-019-1186-3
+#'
+#' @docType data
+#' @name CCLE_chromatin
+#' @usage data(CCLE_chromatin)
+#' @keywords datasets
+#' @format `data.frame` object
+NULL
+
+#' A `data.frame` containing RPPA protein expression data for the Cancer Cell Line
+#' Encyclopedia (CCLE) dataset.
+#'
+#' This is a subset of the RPPA protein expression dataset from Ghandi et al. 2019 that includes
+#' measurements of protein expression levels across a random selection of cell lines.
+#'
+#' @references
+#' Ghandi, M., Huang, F. W., Jané-Valbuena, J., Kryukov, G. V., Lo, C. C., McDonald, E. R., Barretina, J., Gelfand, E. T., Bielski, C. M., Li, H., Hu, K., Andreev-Drakhlin, A. Y., Kim, J., Hess, J. M., Haas, B. J., Aguet, F., Weir, B. A., Rothberg, M. V., Paolella, B. R., … Golub, T. R. (2019). Next-generation characterization of the Cancer Cell Line Encyclopedia. Nature, 569(7757), 503–508. https://doi.org/10.1038/s41586-019-1186-3
+#'
+#' @docType data
+#' @name CCLE_rrpa
+#' @usage data(CCLE_rrpa)
+#' @keywords datasets
+#' @format `data.frame` object
+NULL
+
+#' A `data.frame` containing metabolite expression data for the Cancer Cell Line
+#' Encyclopedia (CCLE) dataset.
+#'
+#' This is a subset of the CCLE metabolomic dataset from Ghandi et al. 2019 that includes
+#' measurements of metabolite expression levels across a random selection of cell lines.
+#'
+#' @references
+#' Ghandi, M., Huang, F. W., Jané-Valbuena, J., Kryukov, G. V., Lo, C. C., McDonald, E. R., Barretina, J., Gelfand, E. T., Bielski, C. M., Li, H., Hu, K., Andreev-Drakhlin, A. Y., Kim, J., Hess, J. M., Haas, B. J., Aguet, F., Weir, B. A., Rothberg, M. V., Paolella, B. R., … Golub, T. R. (2019). Next-generation characterization of the Cancer Cell Line Encyclopedia. Nature, 569(7757), 503–508. https://doi.org/10.1038/s41586-019-1186-3
+#'
+#' @docType data
+#' @name CCLE_metabolomics
+#' @usage data(CCLE_metabolomics)
+#' @keywords datasets
+#' @format `data.frame` object
+NULL
+
+#' A `data.frame` containing metadata for cell lines used in the Cancer Cell
+#' Line Encyclopedia (CCLE) project.
+#'
+#' This is an annotation file obtained from Ghandi et al. 2019 containing a
+#' list of cell line models used, including metadata for each cell line.
+#'
+#' @references
+#' Ghandi, M., Huang, F. W., Jané-Valbuena, J., Kryukov, G. V., Lo, C. C., McDonald, E. R., Barretina, J., Gelfand, E. T., Bielski, C. M., Li, H., Hu, K., Andreev-Drakhlin, A. Y., Kim, J., Hess, J. M., Haas, B. J., Aguet, F., Weir, B. A., Rothberg, M. V., Paolella, B. R., … Golub, T. R. (2019). Next-generation characterization of the Cancer Cell Line Encyclopedia. Nature, 569(7757), 503–508. https://doi.org/10.1038/s41586-019-1186-3
+#'
+#' @docType data
+#' @name CCLE_model_list
+#' @usage data(CCLE_model_list)
+#' @keywords datasets
+#' @format `data.frame` object
+NULL
